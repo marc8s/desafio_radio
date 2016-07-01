178541f2e6a5767b6d3ab40eb48eee258623cf12
marc8s <marcus.ecomp@gmail.com>
2016-06-30 22:07:13 -0300

    Inclusão de metodos no controller dos programas

--- radio_v1/app/Http/Controllers/ProgramasController.php
+++ radio_v1/app/Http/Controllers/ProgramasController.php
@@ -8,7 +8,35 @@ use App\Http\Requests;
 
 class ProgramasController extends Controller
 {
-    public function exibirProgramacao(){		
+    //lista todos os programas
+	public function index(){
+		
+	}
+	//formulário de criação do programa
+	public function create(){
+		
+	}
+	//armazena o programa
+	public function store(){
+		
+	}
+	//mostra um programa especifico
+	public function show($id){
+		
+	}
+	//carregar um programa especifico pra edição
+	public function edit($id){
+		
+	}
+	//alterar um programa especifico 
+	public function update(Request $request, $id){
+		
+	}
+	//deletar um programa especifico 
+	public function destroy($id){
+		
+	}
+	public function exibirProgramacao(){		
 		return view('programacao');
 	}
 	public function irParaHome(){		
