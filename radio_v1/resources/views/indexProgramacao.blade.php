7854c22a12388cd8fed4a7bd8548c5ff12f03718
marc8s <marcus.ecomp@gmail.com>
2016-07-02 14:50:23 -0300

    Mudanças index programacao

--- radio_v1/resources/views/indexProgramacao.blade.php
+++ radio_v1/resources/views/indexProgramacao.blade.php
@@ -8,8 +8,33 @@
                 <div class="panel-heading">Programação</div>
 
                 <div class="panel-body">
+					@if(Session::has('mensagem_sucesso'))
+						<div class="alert alert-sucess">{{Session::get('mensagem_sucesso')}}</div>
+					@endif
                     <h2>Segunda-feira</h2><br/>
+					<table class="table">
+						<th>Nome</th>	
+						<th>Horário</th>	
+						<th>Ações</th>
+						<tbody>
+							@forelse($programacao as $programaescalado)
+							<tr>
+								<td>{{$programaescalado -> nome}}</td>	
+								<td>{{$programaescalado -> horario}}</td>	
+								<td>								
+									<a href="programacao/edit/{{ $programaescalado-> id }}" class="btn btn-default btn-sm">Editar</a>
+									{{ Form::open(['method'=>'DELETE', 'url' => 'programacao/destroy/'.$programaescalado-> id, 'style' => 'display: inline;']) }}
+									<button type="submit" class="btn btn-sm">Remover da Programação</button>		
+									{{ Form::close() }}
+								</td>									
+							</tr>
+							@empty	
+							<p>Nenhum programa escalado!</p>							
+							@endforelse
+						</tbody>
+					</table>
 					<a href="programas/index">Inserir Novo Programa</a><br/>
+					
 					<h2>Terça-feira</h2><br/>
 					<h2>Quarta-feira</h2><br/>
 					<h2>Quinta-feira</h2><br/>
