e8a05503ab25e676c982474b3bd258acd70830b4
marc8s <marcus.ecomp@gmail.com>
2016-07-02 00:47:06 -0300

    Radialistas finalizado

---
+++ radio_v1/resources/views/create-editRadialista.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+					Formulário de gestão de radialista						
+				</div>				
+                <div class="panel-body">
+					@if(Session::has('mensagem_sucesso'))
+						<div class="alert alert-sucess">{{Session::get('mensagem_sucesso')}}</div>
+					@endif
+					
+					@if(Request::is("radialistas/edit/*"))
+						{{Form::model($radialista, [ 'method' => 'PATCH', 'url' => 'radialistas/update/'.$radialista->id])}}
+					@else
+						{{ Form::open(['url' => 'radialistas/store']) }}
+					@endif					
+					
+					{{Form::label('nome', 'Nome')}}
+					{{Form::input('text', 'nome', null, ['class' => 'form-control', 'autofocus', 'placeholder' => 'Nome do Radialista'])}}
+															
+					{{Form::submit('salvar', ['class' => 'btn btn-primary'] )}}
+						
+					{{ Form::close() }}
+					
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
